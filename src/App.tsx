4bd3d4b2133c648d38c722be3a6fafc6cccc9e95
l2chmnl <l2chmnl@gmail.com>
2024-11-08 10:32:17 +0900

    ~78p

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
-
-function Potato() {
-  return <h1>I like potato</h1>
+function Food(props: any) {
+  /* 중괄호로 감싸면 컴포넌트 명으로 사용 가능 */ 
+  return <h1>I like { props.fav }</h1> 
 }
 
 function App() {
   return (
   
   <div>
-    <h1>Hello</h1>
-    <Potato />
+    <h1>Hello</h1> 
+    {/* 문자열을 제외하고 중괄호{} 안에 넣어야함 */}
+    <Food fav="kimchi"/>
+    <Food fav="ramen"/>
+    <Food fav="samgiopsal"/>
+    <Food fav="chukumi"/>
     </div>
   );
 }
