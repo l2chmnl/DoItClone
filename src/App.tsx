1d1ca6aff165648657b011590ccf59d3ad222090
l2chmnl <l2chmnl@gmail.com>
2024-11-08 10:09:56 +0900

    "Day 1"

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+function Potato() {
+  return <h1>I like potato</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <h1>Hello!</h1>
-      </header>
+  
+  <div>
+    <h1>Hello</h1>
+    <Potato />
     </div>
   );
 }
